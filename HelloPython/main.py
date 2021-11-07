f4ba989de99c722d4d9e94fd2c616f763db2734a
grombor <gromgaming@yahoo.com>
2021-11-07 10:45:03 +0100

    added Pymacro

--- HelloPython/main.py
+++
@@ -1,7 +0,0 @@
-import random
-
-print("The first random number is: " + str(random.randrange(100)))
-print("The second random number is: " + str(random.randrange(100)))
-
-for i in range(2):
-    print("s")
