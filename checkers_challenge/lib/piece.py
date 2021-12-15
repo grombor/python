884234626a3b104bdffbfeaaafe819ca43c17cd6
grombor <gromgaming@yahoo.com>
2021-12-15 20:39:27 +0100

    added piece uniicode symbols

--- checkers_challenge/lib/piece.py
+++ checkers_challenge/lib/piece.py
@@ -1,5 +1,8 @@
 class Piece:
-    """ A class for pieces. """
+    """ A class for piece object. """
+
+    piece_symbol = ""
+
 
     def __init__(self, x, y, white=False, is_king=False, jump=False, direction=False, active=True):
         self.x = x                          # The X position of the piece on the board
@@ -7,13 +10,32 @@ class Piece:
         self.is_white = white               # The color of the piece
         self.is_king = is_king              # If True, it means that the piece reach the king row and become kinged
         self.has_jump = jump                # If True the piece has to jump
-        self.move_direction = direction     # need idea here (2-4 directions of movement here)
+        self.move_direction = direction     # need idea here (2-4 directions of movement here) maybe 'L'eft - 'R'ight; 'U'p - 'D'Down
         self.is_active = active             # If True means the piece in game
+
     
     # function made for debugging only
     def __str__(self):
         return f"This piece is:\nat position x: {self.x} y: {self.y}\nwhite color: {self.is_white}\nkinged: {self.is_king}\nhas to jump:{self.has_jump}\nmoving direction: {self.move_direction}\nin game: {self.is_active}"
 
+    
     # Moving function prototype
-    def move(self):
-        return f"moving piece to {self.x}, {self.y}"
+    def move(self, x, y):
+        return print(f"moving piece to {x}, {y}")
+
+    
+    def symbol(self):
+        global piece_symbol
+        if self.is_white: 
+            if not self.is_king:
+                piece_symbol = "\u26c0"
+            else:
+                piece_symbol = "\u26c1"
+        if not self.is_white:
+            if not self.is_king:
+                piece_symbol = "\u26c2"
+            else:
+                piece_symbol = "\u26c3"
+        return piece_symbol
+        
+
--- checkers_challenge/main.py
+++ checkers_challenge/main.py
@@ -3,3 +3,6 @@ from lib import piece
 p1 = piece.Piece(1,1)
 
 print(p1.__str__())
+
+p1.move(1,2)
+print(p1.symbol())
