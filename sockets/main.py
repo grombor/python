4c0017d89454fb3584328c1ecfa19c1601b3c387
grombor <gromgaming@yahoo.com>
2022-04-28 22:53:40 +0200

    initial commit

---
+++ sockets/main.py
@@ -0,0 +1,25 @@
+# Usefull links:
+# https://realpython.com/python-sockets/#background
+# https://www.programiz.com/python-programming/online-compiler/
+# https://jsonformatter.curiousconcept.com/#
+# https://www.geeksforgeeks.org/tcp-3-way-handshake-process/
+
+
+# Simple echo server
+
+import socket
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 5001  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
